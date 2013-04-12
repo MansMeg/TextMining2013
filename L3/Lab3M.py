3873281a1a8ad2dc58f07e23a4d877755e9d9939
Måns Magnusson <mons.magnusson@gmail.com>
2013-04-12 15:37:53 +0100

    Mer kod

--- L3/Lab3M.py
+++ L3/Lab3M.py
@@ -12,6 +12,8 @@ import re
 # pprint(locals())
 # help("BASICMETHODS")
 
+
+# Part 1)
 url = "http://www.guardian.co.uk/politics/2013/apr/08/iron-lady-margaret-thatcher"
 html = urlopen(url).read()
 html[:60]
@@ -24,8 +26,26 @@ raw = re.sub("\n+","\n",raw)
 raw = re.sub("( \n)+"," \n",raw)
 
 raw = nltk.clean_html(html)
+raw = re.sub("\|"," ",raw)
 raw = re.sub("(\s)\s+","\1",raw)
-raw = re.sub("[\t\r\x01]","\n",raw)
-raw
+raw = re.sub("[\t\r\x01\n]",".\n ",raw)
+
+# Part 2)
+porter = nltk.PorterStemmer()
+sentToken = []
+Tokens = []
+for sentence in sents:
+    token = nltk.word_tokenize(sentence)
+    token = [porter.stem(t) for t in token]
+    sentToken.append(token)
+    Tokens = Tokens + token
+
+# Tokens starting with capital letter
+cleanToken = filter(lambda t: re.match("[A-Z]\w+",t) ,Tokens)
+
+raw = re.sub("\|"," ",raw)
+len(set(cleanToken))
 
 
+
+# Part 3)
